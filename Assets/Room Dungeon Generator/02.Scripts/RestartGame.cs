fe07c63d8e4398a2006fc8b1b5689eae62743725
Jalba12042 <jackalba12@icloud.com>
2024-05-10 16:10:30 -0400

    texture "fix"

--- Assets/Room Dungeon Generator/02.Scripts/RestartGame.cs
+++ Assets/Room Dungeon Generator/02.Scripts/RestartGame.cs
@@ -1,28 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    void Update()
     {
-        if (collision.gameObject.tag == "Player")
+        // Check if "R" key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            // Reset time scale in case it was set to 0
-            Time.timeScale = 1f;
-        }
-       
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            // Reset time scale in case it was set to 0
-            Time.timeScale = 1f;
+            // Reload current scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
